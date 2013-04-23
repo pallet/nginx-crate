c54cfbbc9f549187d270af9f50c2990af1782fa8
Ryan Stradling <ryanstradling@gmail.com>
2013-04-23 16:47:06 -0400

    Updated to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.strad/nginx-crate "0.8.0"
+(defproject org.clojars.strad/nginx-crate "0.8.1-SNAPSHOT"
   :description "Create for nginx installation"
   :url "http://github.com/rstradling/nginx-crate"
   :license {:name "Eclipse Pulic License"
