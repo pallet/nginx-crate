b6081ef2cbf670f36affc3a62e5c7b05f0c0d258
Ryan Stradling <ryanstradling@gmail.com>
2013-08-14 15:21:10 -0400

    Preparing for 0.8.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.strad/nginx-crate "0.8.4-SNAPSHOT"
+(defproject org.clojars.strad/nginx-crate "0.8.4"
   :description "Create for nginx installation"
   :url "http://github.com/rstradling/nginx-crate"
   :license {:name "Eclipse Pulic License"
